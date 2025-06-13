677c1855cbd8ac167a377136e7e27079bdf27b8e
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-06-13 23:27:57 +0530

    Update tekken.py

--- src/mistral_common/tokens/tokenizers/tekken.py
+++ src/mistral_common/tokens/tokenizers/tekken.py
@@ -235,7 +235,7 @@ class Tekkenizer(Tokenizer):
             )
             # Tokenizer > v7 should find special tokens in the tokenizer file
             if version > TokenizerVersion("v7"):
-                raise ValueError(err_msg)
+                raise ValueError(" ")
             else:
                 warnings.warn(
                     err_msg,
