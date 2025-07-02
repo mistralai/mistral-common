4f0a006c058358dd78c418f1c33ea72f73227cf8
Patrick von Platen <patrick.v.platen@gmail.com>
2025-07-02 15:59:15 +0200

    clean

---
+++ src/mistral_common/multimodal.py
@@ -0,0 +1,3 @@
+# This file is deprecated. TODO(Patrick) - as soon as vllm & transformers have adapted
+# to new naming, throw deprecation warning and remove this file.
+from mistral_common.image import *  # noqa: F403
