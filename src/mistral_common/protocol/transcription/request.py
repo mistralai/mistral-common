24e8eae881cb38cb4fb9da522888326f263f7f21
Patrick von Platen <patrick.v.platen@gmail.com>
2026-02-12 11:47:39 +0100

    WIP

--- src/mistral_common/protocol/transcription/request.py
+++ src/mistral_common/protocol/transcription/request.py
@@ -59,8 +59,7 @@ class TranscriptionRequest(BaseCompletionRequest):
             "When streaming is enabled, the target streaming delay (in milli-seconds). "
             "This controls how much latency will the model be requested to target after "
             "it hears a word. Note: this is not supported by all models and model targets "
-            "the target but may not strictly meet it. It is also only supported in the "
-            "local-model implementation for inference."
+            "the target but may not strictly meet it."
         ),
     )
 
